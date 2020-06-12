5273eef0cfa7ec65b2d573074edc15be1bbba983
Arnab Maji <arnabmaji1999@gmail.com>
2020-06-12 15:56:16 +0530

    create row layout for study list recycler view

--- app/src/main/java/com/cccexamstudy/pdf_book/MainActivity.java
+++ app/src/main/java/com/cccexamstudy/pdf_book/MainActivity.java
@@ -3,15 +3,18 @@ package com.cccexamstudy.pdf_book;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView studyListRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        studyListRecyclerView = findViewById(R.id.studyListRecyclerView);
 
     }
 }
