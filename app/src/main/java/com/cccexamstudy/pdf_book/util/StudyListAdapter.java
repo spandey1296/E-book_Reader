51792ca8e5291f7e4610254b5572121ea327e010
Arnab Maji <arnabmaji1999@gmail.com>
2020-06-12 19:56:11 +0530

    create activity to show selected pdf

--- app/src/main/java/com/cccexamstudy/pdf_book/util/StudyListAdapter.java
+++ app/src/main/java/com/cccexamstudy/pdf_book/util/StudyListAdapter.java
@@ -1,16 +1,17 @@
 package com.cccexamstudy.pdf_book.util;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cccexamstudy.pdf_book.R;
+import com.cccexamstudy.pdf_book.StudyViewActivity;
 import com.cccexamstudy.pdf_book.model.StudyFile;
 
 import java.util.List;
@@ -42,7 +43,12 @@ public class StudyListAdapter extends RecyclerView.Adapter<StudyListAdapter.Stud
         holder.titleTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, studyFile.getFileName(), Toast.LENGTH_SHORT).show();
+
+                // show pdf view activity
+                Intent intent = new Intent(context, StudyViewActivity.class);
+                intent.putExtra("pdfName", studyFile.getFileName());
+                context.startActivity(intent);
+
             }
         });
     }
